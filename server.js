6844849c29a013a7d587a495c95ef0e3ea01c034
Niranjan VM <niranjanvm739@gmail.com>
2025-04-30 20:42:17 +0530

    Update MongoDB connection to Atlas

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('Failed to connect to MongoDB', err));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
